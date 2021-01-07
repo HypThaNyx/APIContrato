eb4ab568a4423b001ac911da93fea2b20a6e85cd
Wesley Yago da Silva <wesleyyds@hotmail.com>
2021-01-07 15:50:37 -0300

    fix: switch Swagger to Empty route prefix

--- APIService/Startup.cs
+++ APIService/Startup.cs
@@ -41,7 +41,11 @@ namespace APIService
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "APIService v1"));
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "APIService v1");
+                    c.RoutePrefix = string.Empty;
+                });
             }
 
             app.UseHttpsRedirection();
