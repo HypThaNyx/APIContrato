8fc334c8eda6a6afa425cfcd6a5f0ba01b5b1e75
Wesley Yago da Silva <wesleyyds@hotmail.com>
2021-01-11 14:02:27 -0300

    feat: adapt controller to installment creation

--- APIService/Controllers/ContractController.cs
+++ APIService/Controllers/ContractController.cs
@@ -4,23 +4,27 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using APIService.Data;
 using APIService.Models;
+using APIService.Services;
 
 namespace APIService.Controllers
 {
     [ApiController]
-    [Route("contrato")]
+    [Route("")]
     public class ContractController : ControllerBase
     {
         private DataContext _context;
+        private IPrestacaoService _service;
 
         public ContractController(
-            DataContext context)
+            DataContext context,
+            IPrestacaoService service)
         {
             _context = context;
+            _service = service;
         }
 
         [HttpGet]
-        [Route("")]
+        [Route("contrato")]
         public async Task<ActionResult<List<Contrato>>> GetContracts()
         {
             var contracts = await _context.Contratos.ToListAsync();
@@ -28,12 +32,18 @@ namespace APIService.Controllers
         }
 
         [HttpPost]
-        [Route("")]
+        [Route("contrato")]
         public async Task<ActionResult<Contrato>> PostContract(
             [FromBody] Contrato model)
         {
             if (ModelState.IsValid)
             {
+                List<Prestacao> prestacoes = await _service.GerarPrestacoes(model);
+                foreach (Prestacao prestacao in prestacoes)
+                {
+                    _context.Prestacoes.Add(prestacao);
+                }
+                model.Prestacoes = prestacoes;
                 _context.Contratos.Add(model);
                 await _context.SaveChangesAsync();
                 return model;
