8f5b5148cd76e3cffab87ec7f4091ca6c0d6fd76
Wesley Yago da Silva <wesleyyds@hotmail.com>
2021-01-11 16:59:51 -0300

    fix: change controller routes

--- APIService/Controllers/ContractController.cs
+++ APIService/Controllers/ContractController.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using APIService.Data;
 using APIService.Models;
 using APIService.Services;
+using System.Linq;
 
 namespace APIService.Controllers
 {
@@ -24,28 +25,59 @@ namespace APIService.Controllers
 
         [HttpGet]
         [Route("contrato")]
-        public async Task<ActionResult<List<Contrato>>> GetContracts()
+        public async Task<List<Contrato>> GetContracts()
         {
             var contracts = await _context.Contratos.ToListAsync();
-            return contracts;         
+            foreach (Contrato contract in contracts)
+            {
+                contract.Prestacoes = await GetPrestacoesByContrato(contract.Id);
+                _context.Contratos.Update(contract);
+            }
+            return contracts;
+        }
+
+        [HttpGet]
+        [Route("contrato/{id:int}")]
+        public async Task<Contrato> GetContractById(int id)
+        {
+            var contrato = await _context.Contratos.Include(x => x.Id)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == id);
+            return contrato;
+        }
+
+        [HttpGet]
+        [Route("prestacao/{idContrato:int}")]
+        public async Task<List<Prestacao>> GetPrestacoesByContrato(int idContrato)
+        {
+            var prestacoes = await _context.Prestacoes
+                .Include(x => x.Contrato)
+                .AsNoTracking()
+                .Where(x => x.IdContrato == idContrato)
+                .ToListAsync();
+            return prestacoes;
         }
 
         [HttpPost]
         [Route("contrato")]
-        public async Task<ActionResult<Contrato>> PostContract(
-            [FromBody] Contrato model)
+        public async Task<ActionResult<Contrato>> PostContract([FromBody] Contrato model)
         {
             if (ModelState.IsValid)
             {
+                _context.Contratos.Add(model);
+                await _context.SaveChangesAsync();
+
                 List<Prestacao> prestacoes = await _service.GerarPrestacoes(model);
                 foreach (Prestacao prestacao in prestacoes)
                 {
                     _context.Prestacoes.Add(prestacao);
                 }
-                model.Prestacoes = prestacoes;
-                _context.Contratos.Add(model);
+
+                Contrato contrato = await GetContractById(model.Id);
+                contrato.Prestacoes = prestacoes;
+                _context.Contratos.Update(contrato);
                 await _context.SaveChangesAsync();
-                return model;
+                return contrato;
             }
             else
             {
