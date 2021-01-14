6964f1a8143bf5a49126f9778fab8b4ff8b89203
Wesley Yago da Silva <wesleyyds@hotmail.com>
2021-01-14 15:36:29 -0300

    fix: initialize unit testing first file

---
+++ APITesting/ContractControllerTests.cs
@@ -0,0 +1,20 @@
+using APIService.Data;
+using APIService.Models;
+using APIService.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace APITesting
+{
+    public class ContractControllerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
