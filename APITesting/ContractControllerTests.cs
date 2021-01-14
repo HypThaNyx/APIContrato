cecaa39f857a4f95d9185ed060ec4cfdc66c6554
Wesley Yago da Silva <wesleyyds@hotmail.com>
2021-01-14 16:18:08 -0300

    test: create first successful unit testing

--- APITesting/ContractControllerTests.cs
+++ APITesting/ContractControllerTests.cs
@@ -12,13 +12,36 @@ namespace APITesting
     public class ContractControllerTests
     {
         [Fact]
-        public void Test1()
+        public async Task GerarNovoContrato_ContratoValido_DadosValidos()
         {
+            var options = await CreateInMemoryDB();
 
+            //Create mocked Context by seeding Data
+            using (var context = new DataContext(options))
+            {
+                context.Contratos.Add(new Contrato
+                {
+                    QuantidadeParcelas = 5,
+                    ValorFinanciado = 5000
+                });
+                context.SaveChanges();
+                
+                PrestacaoService prestacaoService = new PrestacaoService(context);
+                var contratos = await prestacaoService.ListContratos();
+
+                //ASSERT
+                Assert.NotEmpty(contratos);
+                foreach (var contrato in contratos)
+                {
+                    Assert.Equal(5, contrato.QuantidadeParcelas);
+                    Assert.Equal(5000, contrato.ValorFinanciado);
+                }
+            }
         }
 
-        private DbContextOptions<DataContext> CreateInMemoryDB()
+        private async Task<DbContextOptions<DataContext>> CreateInMemoryDB()
         {
+            await Task.Delay(0);
             return new DbContextOptionsBuilder<DataContext>()
                 .UseInMemoryDatabase(databaseName: "ContractDatabase")
                 .Options;
